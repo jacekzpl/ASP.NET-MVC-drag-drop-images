6c62ddb24e6be86dcfa8d77e17e4a54b4408cbaa
Jacek Zakrzewski <jacek.zakrzewski@bmpconsulting.pl>
2021-12-22 22:48:18 +0100

    Add project files.

---
+++ WebApplication2/GenericHandler.ashx
@@ -0,0 +1,1 @@
+﻿<%@ WebHandler Language="C#" CodeBehind="GenericHandler.ashx.cs" Class="WebApplication2.GenericHandler" %>
---
+++ WebApplication2/GenericHandler.ashx.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication2
+{
+    public class GenericHandler : IHttpHandler
+    {
+        public void ProcessRequest(HttpContext context)
+        {
+            context.Response.ContentType = "text/plain";
+            string dirFullPath = "c:\\Img";
+            string[] files;
+            int numFiles;
+            files = System.IO.Directory.GetFiles(dirFullPath);
+            numFiles = files.Length;
+            numFiles = numFiles + 1;
+            string str_image = "";
+
+            foreach (string s in context.Request.Files)
+            {
+                HttpPostedFile file = context.Request.Files[s];
+                string fileName = file.FileName;
+                string fileExtension = file.ContentType;
+
+                if (!string.IsNullOrEmpty(fileName))
+                {
+                    fileExtension = Path.GetExtension(fileName);
+                    str_image = "image_" + numFiles.ToString() + fileExtension;
+                    string pathToSave = dirFullPath +"\\"+ str_image;
+                    file.SaveAs(pathToSave);
+                }
+            }
+            context.Response.Write(str_image);
+        }
+
+        public bool IsReusable
+        {
+            get
+            {
+                return true;
+            }
+        }
+    }
+}
